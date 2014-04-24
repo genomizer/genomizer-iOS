71666d76fa9acadac95bc712500ebe90b763fe9e
Joel Viklund <c11jvd@mcmaster.dklab.cs.umu.se>
2014-04-24 15:56:28 +0200

    Initial Commit

---
+++ InLogger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InLogger
+//
+//  Created by Joel Viklund on 24/04/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
