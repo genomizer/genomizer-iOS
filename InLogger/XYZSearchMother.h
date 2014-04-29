48745ccff4fae829c98d1ea7cd864818ffc89a36
Joel Viklund <c11jvd@mcmaster.dklab.cs.umu.se>
2014-04-29 10:55:57 +0200

    search mother initiated, please rename!

---
+++ InLogger/XYZSearchMother.h
@@ -0,0 +1,16 @@
+//
+//  XYZSearchMother.h
+//  InLogger
+//
+//  Created by Joel Viklund on 29/04/14.
+//  Copyright (c) 2014 Joel Viklund. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZSearchMother : NSObject
+
+- (void) setVisibleAnnotations: NSArray;
+- (NSString*) getDescription: NSString;
+
+@end
--- InLogger/XYZSearchMother.m
+++ InLogger/XYZSearchMother.m
@@ -1,19 +1,20 @@
 //
-//  XYZSearchResult.m
+//  XYZSearchMother.m
 //  InLogger
 //
 //  Created by Joel Viklund on 29/04/14.
 //  Copyright (c) 2014 Joel Viklund. All rights reserved.
 //
 
-#import "XYZSearchResult.h"
+#import "XYZSearchMother.h"
 
-@interface XYZSearchResult ()
+@interface XYZSearchMother ()
 
+@property NSMutableArray *annotations;
 @property NSMutableArray *data;
 
 @end
 
-@implementation XYZSearchResult
+@implementation XYZSearchMother
 
 @end
