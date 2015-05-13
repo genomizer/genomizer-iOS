47f67d43378dc08b9d4753a9229a305f4eece497
Mattias Scherer <c12msr@mcfluffy.dklab.cs.umu.se>
2015-05-13 14:54:45 +0200

    Done 2 test for using the settings button. One were the done button is pressed and the other with cancel button pressed.

--- InLoggerTests/test automation/Setting.js
+++ InLoggerTests/test automation/Setting.js
@@ -3,16 +3,19 @@
 // Test if settings will show.
 test("Test 1", function(target, app) {
      var window = app.mainWindow();
-     var url = "http://dumbledore.cs.umu.se:7000/";
-     var didShowAlert = false;
+     var didChangeUrl = false;
      UIALogger.logDebug("Now setting up the alert function");
-     
+
      UIATarget.onAlert = function onAlert(alert){
         var title = alert.name();
-        //UIALogger.logWarning("Alert with title ’" + title + "’ encountered!");
-        if (title == "Settings") {
-            return true;
+        alert.logElementTree();
+        var url = alert.scrollViews()[0].tableViews()[0].cells()[0].textFields()[0].value();
+     if (title = "Settings") {
+        if (url == "Test1/") {
+            didChangeUrl = true;
         }
+        return true;
+     }
             return false; // use default handler
         }
      
@@ -20,11 +23,46 @@ test("Test 1", function(target, app) {
      UIALogger.logMessage( "Select settings" );
      window.buttons()["Setting"].tap();
      target.delay(2);
-     //app.keyboard().keys()["Delete"].touchAndHold(3.6);
-     //app.keyboard().typeString("test");
-     app.alert().textFields()[0].setValue("Test");
+     
+     app.alert().scrollViews()[0].tableViews()[0].cells()[0].textFields()[0].setValue("Test1/");
      app.alert().buttons()["Done"].tap();
      
      window.buttons()["Setting"].tap();
-     assertTrue(didShowAlert);
+     target.delay(2);
+     app.alert().buttons()["Done"].tap();
+     assertTrue(didChangeUrl);
 });
+
+test("Test 2", function(target, app) {
+     var window = app.mainWindow();
+     var didChangeUrl = false;
+     UIALogger.logDebug("Now setting up the alert function");
+     
+     UIATarget.onAlert = function onAlert(alert){
+     var title = alert.name();
+     alert.logElementTree();
+     var url = alert.scrollViews()[0].tableViews()[0].cells()[0].textFields()[0].value();
+     if (title = "Settings") {
+     if (url == "Test1/") {
+     didChangeUrl = true;
+     }
+     return true;
+     }
+     return false; // use default handler
+     }
+     
+     app.logElementTree();
+     UIALogger.logMessage( "Select settings" );
+     window.buttons()["Setting"].tap();
+     target.delay(2);
+     
+     app.alert().scrollViews()[0].tableViews()[0].cells()[0].textFields()[0].setValue("");
+     app.alert().buttons()["Cancel"].tap();
+     
+     window.buttons()["Setting"].tap();
+     target.delay(2);
+     app.alert().buttons()["Done"].tap();
+     assertTrue(didChangeUrl);
+});
+
+
