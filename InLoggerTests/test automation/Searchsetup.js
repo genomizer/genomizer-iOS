8091da4ae1b26e88381aaa86409d26805a62d195
Mattias Scherer <c12msr@mcfluffy.dklab.cs.umu.se>
2015-05-25 15:36:00 +0200

    Select file test, created searchsetup

--- InLoggerTests/test automation/Searchsetup.js
+++ InLoggerTests/test automation/Searchsetup.js
@@ -1,14 +1,12 @@
-#import "Loginsetup.js"
-
 function search() {
     var target = UIATarget.localTarget();
-    login();
+    target.delay(0.1);
+    target.frontMostApp().mainWindow().tableViews()[0].cells()["Experiment"].tap();
+    target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.40, y:0.86}});
     target.frontMostApp().mainWindow().tableViews()[0].cells()["Experiment"].textFields()["Experiment"].textFields()["Experiment"].tap();
-    target.frontMostApp().windows()[1].pickers()[0].wheels()[0].tapWithOptions({tapOffset:{x:0.47, y:0.39}});
+    target.frontMostApp().windows()[1].pickers()[0].wheels()[0].tapWithOptions({tapOffset:{x:0.41, y:0.56}});
+    target.delay(0.4);
     target.frontMostApp().windows()[1].toolbar().buttons()["Done"].tap();
     target.frontMostApp().mainWindow().buttons()["Search"].tap();
-    
-    
 }
-
 
